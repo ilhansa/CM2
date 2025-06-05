120390b07c013f60ceb90b92a1c8508177397b18
Yulia Nadzifah <ilhansalihilsa@gmail.com>
2025-06-05 15:39:02 +0700

    pasien

--- Pasien.java
+++ Pasien.java
@@ -1,3 +1,17 @@
 public class Pasien {
-    
+    String nama;
+    String nik;
+    String keluhan;
+
+    Pasien(String nama, String nik, String keluhan) {
+        this.nama = nama;
+        this.nik = nik;
+        this.keluhan = keluhan;
+    }
+
+    void TampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Nik: " + nik);
+        System.out.println("Keluhan: " + keluhan);
+    }
 }
